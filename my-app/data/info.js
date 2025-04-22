835ce0d84dd2408b4376f7fdf3aa80e2fba2af84
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-22 13:27:42 -0300

    first commit

---
+++ my-app/data/info.js
@@ -0,0 +1,19 @@
+export const info = [
+    {
+        id: 1,
+        title: "Por do Sol",
+        image: "https://cursinhoparamedicina.com.br/wp-content/uploads/2022/10/Paisagem-1.jpg",
+    },
+    {
+        id: 2,
+        title: "Amazonas",
+        image: "https://static.mundoeducacao.uol.com.br/mundoeducacao/2020/10/floresta-amazonica.jpg",
+    },
+    {
+        id: 3,
+        title: "Praia",
+        image: "https://www.agenciasp.sp.gov.br/wp-content/uploads/2025/02/Praia-do-Felix-1024x768.jpg",
+
+        
+      },
+]
